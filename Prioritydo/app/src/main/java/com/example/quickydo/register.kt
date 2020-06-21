fc3af54a7f811ef068cfcf5d0b4254f5ba538f0e
Dalia <47950134+DaliaW@users.noreply.github.com>
2020-06-22 01:32:55 +0200

    updated small things

--- Prioritydo/app/src/main/java/com/example/quickydo/register.kt
+++ Prioritydo/app/src/main/java/com/example/quickydo/register.kt
@@ -2,15 +2,23 @@ package com.example.quickydo
 
 import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 
 class register : AppCompatActivity() {
-
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
+        var emailreg: EditText
+        var passreg: EditText
+        var regButton: Button
+        var alreadyacc: ImageView
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         getWindow().setFlags(
